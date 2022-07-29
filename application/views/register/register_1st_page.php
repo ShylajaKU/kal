97052c702028818908218cd4659e0e26d96839d8
Vishnu I P <107356936+ShylajaKU@users.noreply.github.com>
2022-07-28 21:47:52 -0700

    Update register_1st_page.php

--- application/views/register/register_1st_page.php
+++ application/views/register/register_1st_page.php
@@ -22,70 +22,75 @@
 <script src="<?= base_url('assets/js/selectize.min.js')?>"></script>
 
 <br>
-
 <div class="asd" >
+    <div>
+        <h3> Register for free at k4kalyanam.in</h3><br>
+    </div>
 <?= form_open() ?>
     <!-- ------------------------------------- -->
-    <!-- <label for="" class="form-label">Profile created by *</label>
-    <select class="form-control" name="" id="" required>
-        <option value="" selected disabled>Profile Created By *</option>
-        <option value="self">Self</option>
-        <option value="parents">Parents</option>
-        <option value="sibling">Sibling</option>
-        <option value="relative">Relative</option>
-        <option value="friend">Friend</option>
-    </select><br> -->
-    <!-- ------------------------------------- -->
     <label for="" class="form-label">Profile created by *</label><br>
-    <input type="radio" class="btn-check" name="created_by" id="option1" autocomplete="off" value="self">
-    <label class="btn bg-col-1" for="option1">Self</label>
+    <input type="radio" class="btn-check" name="created_by" id="self" autocomplete="off" value="self">
+    <label class="btn bg-col-1" for="self">Self</label>
 
-    <input type="radio" class="btn-check" name="created_by" id="option2" autocomplete="off" value="parents">
-    <label class="btn bg-col-1" for="option2">Parents</label>
+    <input type="radio" class="btn-check" name="created_by" id="parents" autocomplete="off" value="parents">
+    <label class="btn bg-col-1" for="parents">Parents</label>
 
-    <input type="radio" class="btn-check" name="created_by" id="option3" autocomplete="off" value="sibling">
-    <label class="btn bg-col-1" for="option3">Sibling</label>
+    <input type="radio" class="btn-check" name="created_by" id="sibling" autocomplete="off" value="sibling">
+    <label class="btn bg-col-1" for="sibling">Sibling</label>
 
-    <input type="radio" class="btn-check" name="created_by" id="option4" autocomplete="off" value="friend">
-    <label class="btn bg-col-1" for="option4">Friend</label>
+    <input type="radio" class="btn-check" name="created_by" id="friend" autocomplete="off" value="friend">
+    <label class="btn bg-col-1" for="friend">Friend</label>
 <br><br>
     <!-- ------------------------------------- -->
     <label class="form-label" for="">Name of Bride/Groom *</label>
     <input class="form-control" type="text" required><br>
     <!-- ------------------------------------- -->
     <label for="" class="form-label">Gender *</label> <br>
-    <input type="radio" class="btn-check" name="gender" id="option1" autocomplete="off" value="male">
-    <label class="btn bg-col-1" for="option1">Male</label>
-    <input type="radio" class="btn-check" name="gender" id="option2" autocomplete="off" value="female">
-    <label class="btn bg-col-1" for="option2">Female</label>
+    <input type="radio" class="btn-check" name="gender" id="male" autocomplete="off" value="male">
+    <label class="btn bg-col-1" for="male">Male</label>
+    <input type="radio" class="btn-check" name="gender" id="female" autocomplete="off" value="female">
+    <label class="btn bg-col-1" for="female">Female</label>
 <br><br>
     <!-- ------------------------------------- -->
     <label class="form-label"  for="">Date of Birth *</label>
     <input class="form-control" style="width:300px ;" type="date" name="dob" id="" required><br>
     <!-- ------------------------------------- -->
     <label for="" class="form-label">Marital Status *</label><br>
-    <input type="radio" class="btn-check" name="marital_status" id="option1" autocomplete="off">
-<label class="btn bg-col-1" for="option1">Unmarried</label>
+    <input type="radio" class="btn-check" name="marital_status" id="unmarried" autocomplete="off">
+<label class="btn bg-col-1" for="unmarried">Unmarried</label>
 
-<input type="radio" class="btn-check " name="marital_status" id="option2" autocomplete="off">
-<label class="btn bg-col-1" for="option2">Widow/Widower</label>
+<input type="radio" class="btn-check " name="marital_status" id="wid" autocomplete="off">
+<label class="btn bg-col-1" for="wid">Widow/Widower</label>
 
-<input type="radio" class="btn-check" name="marital_status" id="option3" autocomplete="off" >
-<label class="btn bg-col-1" for="option3">Divorced</label>
+<input type="radio" class="btn-check" name="marital_status" id="divorced" autocomplete="off" >
+<label class="btn bg-col-1" for="divorced">Divorced</label>
 
-<input type="radio" class="btn-check" name="marital_status" id="option4" autocomplete="off">
-<label class="btn bg-col-1" for="option4">Seperated</label>
+<input type="radio" class="btn-check" name="marital_status" id="seperated" autocomplete="off">
+<label class="btn bg-col-1" for="seperated">Seperated</label>
 
 <br><br>
 
 
     <!-- ------------------------------------- -->
+    <label for="" class="form-label">Phone Number *</label>
+    <input class="form-control" type="tel" name="phone_no" id="" required placeholder="Please enter a valid phone number">
+    <br>
+
+    <!-- ------------------------------------- -->
+    <label for="" class="form-label">Email ID *</label>
     <input class="form-control" id="email" type="text" name="email" placeholder="Email">
 <br>
     <!-- ------------------------------------- -->
-    <input class="form-control" type="password" name="password" id="pass" placeholder="Password">
+    <label for="" class="form-label">Create Password *</label>
+
+    <input class="form-control" type="password" name="password" id="pass" placeholder="Create a Password">
     <br>
     <!-- ------------------------------------- -->
+    <label for="" class="form-label">Confirm Password *</label>
+
+<input class="form-control" type="password" name="confirm_password" id="pass" placeholder="Confirm Password">
+<br>
+<!-- ------------------------------------- -->
     <button type="submit" class="btn btn-primary vip-center">Register</button>
     <!-- ------------------------------------- -->
     <a style="margin-left:15px ;" href="http://">Login Here</a>
