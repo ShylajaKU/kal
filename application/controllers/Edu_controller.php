fb841f5ad40ad2f19af8a2799a563a68603c334a
Vishnu I P <107356936+ShylajaKU@users.noreply.github.com>
2022-07-30 18:55:51 +0530

    h

--- application/controllers/Edu_controller.php
+++ application/controllers/Edu_controller.php
@@ -10,29 +10,60 @@ public function education_and_job_fc(){
     $query = $this->db->get('highest_education');
     $result = $query->result_array();
     $data['highest_education_list'] = $result;
+
+
+    $this->db->where('approved','1');
+    $query = $this->db->get('professions_list');
+    $result = $query->result_array();
+    $data['professions_list'] = $result;
+
+    $query = $this->db->get('annual_income');
+    $result = $query->result_array();
+    $data['annual_income_list'] = $result;
+
     $this->form_validation->set_rules('highest_education','Highest Education','required' );
     if(!$this->form_validation->run()){
         $this->load->view('templates/head/header');
         $this->load->view('edu/education_and_job_details',$data);
         $this->load->view('templates/foot/footer');
     }else{
-        $he = $this->input->post('highest_education');
-        if($he == '0'){
-        $he_t = $this->input->post('highest_education_typed_in');
+        //=======================================
+        $higher_education = $this->input->post('highest_education');
+        if($higher_education == '0'){
+        $vip = $this->input->post('highest_education_typed_in');
         $data = array(
-            'highest_education' => $he_t,
+            'highest_education' => $vip,
             'created_by' => $this->session->userdata('user_id'),
             'approved' => '0',
             );
-            $this->db->where('highest_education',$he_t);
-            $query1 = $this->db->get('highest_education');
-            echo $count = $query1->num_rows();
+            $this->db->where('highest_education',$vip);
+            $count = $this->db->get('highest_education')->num_rows();
             if(!$count){
                 $this->db->insert('highest_education',$data);
             }
-            $he = $he_t;
+            $higher_education = $vip;
         }
-        // echo $he;
+        //=======================================
+        $profession = $this->input->post('profession');
+        if($profession == '0'){
+            $vip = $this->input->post('profession_typed_in');
+            $data = array(
+                'profession' => $vip,
+                'created_by' => $this->session->userdata('user_id'),
+                'approved' => '0',
+            );
+            $this->db->where('profession',$vip);
+            $count = $this->db->get('professions_list')->num_rows();
+            if(!$count){
+                $this->db->insert('professions_list',$data);
+            }
+            $profession = $vip;
+        }
+        //=======================================
+
+
+
+
     }
 
 
